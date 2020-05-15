416e17625dfba5558ce5a301967384c0362d93b0
Justin Price <mr.price@Justins-iMac.local>
2020-05-15 18:03:02 -0400

    names done/tested

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
-        self.capacity = capacity
-        self.node = None
-        
+        self.capacity= capacity
+        self.data = []
+        current = self.head
+
 
     def append(self, item):
-        pass
+        while current:
+        if len(self.data) == self.data:
+            self.data.pop(current)
+            self.data.insert(current, item)
+        
+            # remove head
+            # insert new item into head
 
     def get(self):
-        pass
+        return self.data
+
